b4ec23b533dcf812a87668f6e721366214105eb5
Sam Deane <sam@elegantchaos.com>
2012-02-27 15:06:59 +0000

    added build scripts

--- Scripts/ios-test.sh
+++ Scripts/ios-test.sh
@@ -4,6 +4,9 @@ base=`dirname $0`
 common="$base/../../ECUnitTests/Scripts/"
 source "$common/test-common.sh"
 
+#xcodebuild -target "ECCoreIOS" -configuration "$testConfig" -sdk "$testSDKiOS" $testOptions | "$common/$testConvertOutput"
+#xcodebuild -target "ECCoreIOSTests" -configuration "$testConfig" -sdk "$testSDKiOS" $testOptions | "$common/$testConvertOutput"
+
 xcodebuild -workspace "ECCore.xcworkspace" -scheme "ECCoreIOS" -configuration "$testConfig" -sdk "$testSDKiOS" test | "$common/$testConvertOutput"
 
 
