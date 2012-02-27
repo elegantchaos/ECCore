083afa5133bb2d41d3967e54f4495916b856808f
Sam Deane <sam@elegantchaos.com>
2012-02-27 18:45:23 +0000

    experimenting with script

--- Scripts/ios-test.sh
+++ Scripts/ios-test.sh
@@ -3,10 +3,14 @@ echo Testing IOS
 base=`dirname $0`
 common="$base/../../ECUnitTests/Scripts/"
 source "$common/test-common.sh"
+xcoderoot=`xcode-select --print-path`
+xcodebuild="$xcoderoot/usr/bin/xcodebuild"
 
+xcodebuild -workspace "ECCore.xcworkspace" -scheme "ECCoreIOS" -configuration "Debug" -sdk "iphonesimulator" test
+
+#"$xcodebuild" -workspace "ECCore.xcworkspace" -scheme "ECCoreIOS" -configuration "$testConfig" -sdk "$testSDKiOS" test | "$common/$testConvertOutput"
 #xcodebuild -target "ECCoreIOS" -configuration "$testConfig" -sdk "$testSDKiOS" $testOptions | "$common/$testConvertOutput"
 #xcodebuild -target "ECCoreIOSTests" -configuration "$testConfig" -sdk "$testSDKiOS" $testOptions | "$common/$testConvertOutput"
 
-xcodebuild -workspace "ECCore.xcworkspace" -scheme "ECCoreIOS" -configuration "$testConfig" -sdk "$testSDKiOS" test | "$common/$testConvertOutput"
 
 
