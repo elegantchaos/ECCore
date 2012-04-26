84dd293e56a431c8f27d90fd56d0ec50a84ee10f
Sam Deane <sam@elegantchaos.com>
2012-04-26 12:10:13 +0100

    Added KVO test

---
+++ Generic/Tests/ECKVOTests.m
@@ -0,0 +1,49 @@
+// --------------------------------------------------------------------------
+//! @author Sam Deane
+//! @date 12/04/2011
+//
+//  Copyright 2012 Sam Deane, Elegant Chaos. All rights reserved.
+//  This source code is distributed under the terms of Elegant Chaos's 
+//  liberal license: http://www.elegantchaos.com/license/liberal
+// --------------------------------------------------------------------------
+
+#import "ECTestCase.h"
+#import "ECKVO.h"
+
+@interface KVOTestClass : NSObject
+
+@property (strong, nonatomic) NSString* name;
+
+@end
+
+@implementation KVOTestClass
+
+@synthesize name = _name;
+
+@end
+
+@interface ECKVOTests : ECTestCase
+
+@end
+
+@implementation ECKVOTests
+
+- (void)testKVO
+{
+	KVOTestClass* test = [[KVOTestClass alloc] init];
+	test.name = @"fred";
+	__block BOOL blockRan = NO;
+	
+	ECObserver* observer = [test addObserverForKeyPath:@"name" options:0 action:^(NSDictionary *change) {
+		blockRan = YES;
+	}];
+	
+	test.name = @"jim";
+	
+	ECTestAssertTrue(blockRan);
+	
+	[test removeObserver:observer];
+	[test release];
+}
+
+@end
