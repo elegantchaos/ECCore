2acb11cb9878ef6648662116d38d44058b7d3a41
Sam Deane <sam@elegantchaos.com>
2014-04-11 17:48:43 +0100

    Updated copyright date.

--- Source/Generic/Tests/NSDictionaryTests.m
+++ Source/Generic/Tests/NSDictionaryTests.m
@@ -3,7 +3,7 @@
 //! @file:
 //! Unit tests for the NSDictionary+ECUtilitiesTests.h category.
 //
-//  Copyright 2013 Sam Deane, Elegant Chaos. All rights reserved.
+//  Copyright 2014 Sam Deane, Elegant Chaos. All rights reserved.
 //  This source code is distributed under the terms of Elegant Chaos's 
 //  liberal license: http://www.elegantchaos.com/license/liberal
 // --------------------------------------------------------------------------
